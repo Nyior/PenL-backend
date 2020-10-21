b85b20907fccb4e5d5e45643a81b0ad3a3b9e7dd
Nyior Clement Jr <cnyior27@gmail.com>
2020-10-21 15:16:45 +0100

    prepared app for heroku deployment

--- PenL_backend/settings.py
+++ PenL_backend/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_env_variable('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
