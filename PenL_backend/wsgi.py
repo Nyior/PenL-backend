28e1c68b7bd062e482f6b3f6c62f498adf3c169a
Nyior Clement Jr <cnyior27@gmail.com>
2020-11-03 11:07:11 +0100

    kai

--- PenL_backend/wsgi.py
+++ PenL_backend/wsgi.py
@@ -13,7 +13,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PenL_backend.settings')
 import socketio
 from django.core.wsgi import get_wsgi_application
 
+django_app = get_wsgi_application()
 from apps.game_room.api.views import sio
 
-django_app = get_wsgi_application()
 application = socketio.WSGIApp(sio, django_app)
