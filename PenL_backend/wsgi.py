93f609a92ad000b303a866ecf98e60247d1ef7a0
Nyior Clement Jr <cnyior27@gmail.com>
2020-11-03 11:02:42 +0100

    i don tire ohh

--- PenL_backend/wsgi.py
+++ PenL_backend/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import socketio
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PenL_backend.settings')
 
+import socketio
 from django.core.wsgi import get_wsgi_application
+
 from apps.game_room.api.views import sio
 
 django_app = get_wsgi_application()
